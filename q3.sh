97c49c3dfbd5b029ab6abee249a7dae0037976ae
devrakaops <dollyjangid40@gmail.com>
2026-05-13 15:55:39 +0530

    Create q3.sh

---
+++ q3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://github.com/Mirantis/cri-dockerd/releases/download/v0.4.3/cri-dockerd_0.4.3.3-0.ubuntu-jammy_amd64.deb
+
+echo ""
+echo ""
+echo "cri-dockerd_0.4.3.3-0.ubuntu-jammy_amd64.deb package available on the PWD"
